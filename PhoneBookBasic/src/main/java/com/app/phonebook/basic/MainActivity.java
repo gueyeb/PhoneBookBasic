122dd76cf9663f72924fe5edb0ee584cf27f83cd
Babacar Gueye <gueyens11@gmail.com>
2013-07-14 13:28:27 +0200

    Modification on MainActivity. ListView
    ListView of all contacts in MainActivity view.

--- PhoneBookBasic/src/main/java/com/app/phonebook/basic/MainActivity.java
+++ PhoneBookBasic/src/main/java/com/app/phonebook/basic/MainActivity.java
@@ -1,15 +1,37 @@
 package com.app.phonebook.basic;
 
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends Activity {
+
+    private ListView lvPhone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lvPhone = (ListView)findViewById(R.id.listPhone);
+
+        List<PhoneBook> listPhoneBook = new ArrayList<PhoneBook>();
+        listPhoneBook.add(new PhoneBook(
+                BitmapFactory.decodeResource(getResources(), R.drawable.avatar_pete),
+                "Pete Houston", "010-9817-6331", "pete.houston.17187@gmail.com"));
+        listPhoneBook.add(new PhoneBook(
+                BitmapFactory.decodeResource(getResources(), R.drawable.avatar_lina),
+                "Lina Cheng", "046-7764-1142", "lina.cheng011@sunny.com"));
+        listPhoneBook.add(new PhoneBook(
+                BitmapFactory.decodeResource(getResources(), R.drawable.avatar_jenny),
+                "Jenny Nguyen", "0913-223-498", "jenny_in_love98@yahoo.com"));
+        PhoneBookAdapter adapter = new PhoneBookAdapter(this, listPhoneBook);
+        lvPhone.setAdapter(adapter);
     }
 
 
